631716a05b28f76e182cd0ba5c9ba5b748cca6fd
rjw0205 <rjw0205@kaist.ac.kr>
2017-05-09 22:23:13 +0900

    mypage 더 꾸밈 / 예약 상세정보 추가했고, 예약 취소 기능 추가함

---
+++ cancel_reservation.js
@@ -0,0 +1,18 @@
+var reservation_info = document.getElementById("reservation_info");
+
+/*
+$('#cancel_reservation').click(function() {
+    alert("haha");
+    reservation_info.remove();
+});
+*/
+
+
+$('#cancel_reservation').click(function() {
+    var result = confirm('정말 취소하시겠습니까?');
+    
+    if(result){
+        reservation_info.remove();
+    }
+});
+
