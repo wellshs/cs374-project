90261b4480a4105457b64341be1d6ee4e86ca5a1
rjw0205 <rjw0205@kaist.ac.kr>
2017-05-21 13:17:36 +0900

    가격 sort 기능 구현

--- db.js
+++ db.js
@@ -1,31 +1,30 @@
-
 var db;
 db = [ {
-    "name" : "ABC academy",
+    "name" : "A academy",
     "location" : "서울시 노원구",
     "real_location" : "서울시 노원구 중계1동 243-5번지",
     "type" : "2종 보통",
-    "price" : "400,000",
+    "price" : 400000,
     "date" : "2017-05-18",
     "url" : "A_academy.html"
   },
   {
-    "name" : "ABC academy",
+    "name" : "A academy",
     "location" : "서울시 노원구",
     "real_location" : "서울시 노원구 중계1동 243-5번지",
     "type" : "2종 보통",
     "date" : "2017-05-16",
-    "price" : "400,000",
+    "price" : 400000,
     "url" : "A_academy.html"
   },
   {
-    "name" : "H academy",
+    "name" : "B academy",
     "location" : "서울시 노원구",
     "real_location" : "서울시 노원구 상계동 127-3번지",
     "type" : "2종 보통",
     "date" : "2017-05-16",
-    "price" : "346,000",
-    "url" : "A_academy.html"
+    "price" : 346000,
+    "url" : "B_academy.html"
   },
   {
     "name" : "C academy",
@@ -33,7 +32,7 @@ db = [ {
     "real_location" : "서울시 노원구 하계2동 307-8번지",
     "type" : "2종 보통",
     "date" : "2017-05-16",
-    "price" : "500,000",
-    "url" : "A_academy.html"
+    "price" : 500000,
+    "url" : "C_academy.html"
   }
 ]
--- filter.js
+++ filter.js
@@ -1,3 +1,6 @@
+var searchlist = [];
+var z = [];
+
 var config = {
   apiKey: "AIzaSyApEs803NuXtJukZzsjBL7SKFqrMHjGjnI",
   authDomain: "cs492-test.firebaseapp.com",
@@ -29,27 +32,72 @@ function myFunction() {
     type = document.getElementById("type").value;
     date = document.getElementById("date").value;
     $("#result").empty();
+    searchlist = [];
 
     for(var i=0; i<db.length; i++){
             if((location == db[i].location) && (type == db[i].type) && (date == db[i].date)){
-                //$("#result").append("<p>" + db[i].name + "</p>");
-                //$("#result").append("<p>" + db[i].real_location + "</p>");
-                //$("#result").append("<p>" + db[i].type + "</p>");
                 x += 1
                 var a = document.createElement('a');
                 var linkText = document.createTextNode(db[i].name);
                 a.appendChild(linkText);
                 a.className += "academyname";
-                // a.addClass("academy");
                 a.href = db[i].url;
-
+                
+                
                 $("#result").append(a);
                 $("#result").append("<p>" + db[i].real_location + "</p>")
                 $("#result").append("<p>" + db[i].type + " : " + db[i].price + "</p>");
+                
+                searchlist.push([a, db[i].real_location, db[i].type, db[i].price]);
+                z.push([a, db[i].real_location, db[i].type, db[i].price]);
             }
         }
     if(x==0){
         document.getElementById("result").innerHTML = "No result";
     }
+    
     bindacademyname();
 }
+
+
+
+
+
+$("#someSwitchOptionPrimary").click(function(){
+    if ($('#onoff').text() == '가격 정렬 OFF'){
+        $('#onoff').text('가격 정렬 ON');
+        
+        $("#result").empty();
+        
+        searchlist.sort(function(a,b) {
+            return a[3] - b[3];
+        });
+        
+        for(var j=0; j<searchlist.length; j++){
+            $("#result").append(searchlist[j][0]);
+            $("#result").append("<p>" + searchlist[j][1] + "</p>");
+            $("#result").append("<p>" + searchlist[j][2] + " : " + searchlist[j][3] + "</p>");
+        }
+    }
+    else{
+        $('#onoff').text('가격 정렬 OFF');
+        
+        $("#result").empty();
+        
+        for(var k=0; k<z.length; k++){
+            $("#result").append(z[k][0]);
+            $("#result").append("<p>" + z[k][1] + "</p>");
+            $("#result").append("<p>" + z[k][2] + " : " + z[k][3] + "</p>");
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
